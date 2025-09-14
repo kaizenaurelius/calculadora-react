fb53b6de8b9d76bef8a1e6a09ca3da56b3534e91
Kaizen Aurelius <rafaeltobass@gmail.com>
2025-09-14 12:05:15 -0500

    Comp Button Styles Comp Button Render Comp Button en App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Button from './components/Button';
 import kaizenLogo from './imgs/ChatGPT Image 1 sept 2025, 13_02_16.png'
 
 function App() {
@@ -15,10 +16,30 @@ function App() {
       </div>
 
       <div className='calculator-container'>
-        <div className='row-calculator'></div>
-        <div className='row-calculator'></div>
-        <div className='row-calculator'></div>
-        <div className='row-calculator'></div>
+        <div className='row-calculator'> 
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className='row-calculator'>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>-</Button>
+        </div>
+        <div className='row-calculator'>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>*</Button>
+        </div>
+        <div className='row-calculator'>
+          <Button>=</Button>
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>/</Button>          
+        </div>
         <div className='row-calculator'></div>
       </div>
     </div>
---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import '../styles/button.css'
+
+function Button(props) {
+  const isOperator = value => {
+    return isNaN(value) && (value !== '.') && (value !== '=');
+  }
+
+
+  return (
+      <div
+        className={`button-container ${isOperator(props.children) ? 'operator' : ''}`.trimEnd()}>
+        {props.children}
+      </div>
+  )
+}
+
+export default Button
